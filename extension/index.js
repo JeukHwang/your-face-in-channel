4bcc1bfc3ab81397fdea81839ff6d98f59f79b77
JeukHwang <jeukhwang.dev@gmail.com>
2023-02-18 04:11:16 +0900

    feat: connect images from controller

--- extension/index.js
+++ extension/index.js
@@ -59,21 +59,44 @@ async function getTextMsgs() {
     return Array.from(textMsgTags);
 };
 
-const emojiMap = (str) => "🔥"; // pseudo!!!
+async function getEmojiUrl(emojiName) {
+    const defaultUrl = "https://channel-emoji.s3.ap-northeast-2.amazonaws.com/channel.png";
+    // const defaultUrl = "https://thumbs.dreamstime.com/b/funny-face-baby-27701492.jpg";
+    const purifiedName = emojiName.slice(1, -1);
+    const baseUrl = "https://e198-115-94-114-198.jp.ngrok.io";
+    const url = `${baseUrl}/emoji/${purifiedName}`;
+    const res = await fetch(new Request(url), {
+        method: "get",
+        headers: new Headers({ "ngrok-skip-browser-warning": "69420", }),
+    });
+    if (res.status === 200) {
+        try {
+            const emojiUrl = await (res.json());
+            if (emojiUrl !== null) {
+                return emojiUrl.inside;
+            }
+        } catch (e) {
+            if (!(e instanceof SyntaxError)) { throw e; }
+        }
+    }
+    return defaultUrl;
+}
 
 async function renderExpr() {
     log(`Start renderExpr`);
     const content = await getTextMsgs();
-    content.forEach(element => {
-        const msg = element.textContent;
-        const expr = msg.match(/;[A-Za-z-]+;/g);
-        if (expr !== null) {
-            log(`${expr.join(" | ")} found in ${msg}`);
-            const textContent = expr.reduce(
-                (textContent_, expr) => textContent_.replace(expr, emojiMap(expr)),
-                element.textContent
-            );
-            element.textContent = textContent;
+    content.forEach(async element => {
+        const msg = element.innerHTML;
+        const exprs = msg.match(/;[A-Za-z-_]+;/g);
+        if (exprs !== null) {
+            const exprToUrl = await Promise.all(exprs.map(expr => getEmojiUrl(expr)));
+            const exprToTag = exprToUrl.map((expr) => `<img class="channel-emoji" src=${expr}>`);
+            log(`${exprs.join(" | ")} found in ${msg}`);
+            let innerHTML = msg;
+            for (let i = 0; i < exprs.length; i++) {
+                innerHTML = innerHTML.replace(exprs[i], exprToTag[i]);
+            }
+            element.innerHTML = innerHTML;
         }
     });
     log(`Finish renderExpr`);
